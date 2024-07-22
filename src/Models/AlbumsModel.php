83a2af089a754594adb8e9325b648ce75979d903
Ivan_Savchuk <vanya.savchuk@gmail.com>
2024-07-22 12:38:15 +0100

    counter function was added

--- src/Models/AlbumsModel.php
+++ src/Models/AlbumsModel.php
@@ -7,4 +7,16 @@ class AlbumsModel
     {
         $this->db = $db;
     }
+    public function getSongCount($albumId): array
+    {
+        $query = $this->db->prepare("SELECT `albums`.`id` AS 'album_id', `songs`.`id` AS 'song_id', COUNT(`songs`.`album_id`)
+FROM `albums`
+INNER JOIN `songs`
+ON `albums`.`id` = `songs`.`album_id`
+WHERE `albums`.`id` = $albumId;");
+        $query->setFetchMode(PDO::FETCH_CLASS, Album::class);
+        $query->execute();
+        return $query->fetch();
+    }
 }
+
